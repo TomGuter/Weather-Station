5a0939da5ebe9d3397d92c0cb31a57f0b7a4c10d
Tom Guter <128406348+TomGuter@users.noreply.github.com>
2024-07-21 09:12:23 +0300

    Delete WeatherStationSimulation/UserInputHandler.h

--- WeatherStationSimulation/UserInputHandler.h
+++
@@ -1,8 +0,0 @@
-//#ifndef USER_INPUT_HANDLER_H
-//#define USER_INPUT_HANDLER_H
-//
-//#include <atomic>
-//
-//void userInputHandler(std::atomic<bool>& running);
-//
-//#endif // USER_INPUT_HANDLER_H
